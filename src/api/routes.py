4e19e29b8dff4766e41514a2dcdde54febeb791e
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-12 19:24:41 +0000

    begin to create the routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,13 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Create the JWT
+@api.route('/token', methods=['POST'])
+def create_token():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
