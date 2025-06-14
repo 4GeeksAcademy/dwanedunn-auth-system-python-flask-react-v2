949b30e86db817111cfe5de3c3ebdfc6a5ca8c1f
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-14 20:38:43 +0000

    add log out

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,16 +1,25 @@
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 
+
 export const Navbar = () => {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+
+		(window.sessionStorage.getItem('accessToken') ? setIsAuthenticated(true) : setIsAuthenticated(false))
+	}, []);
 
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1 ">Home</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/login">
-						<button className="btn btn-primary">Login</button>
+						<button className="btn btn-primary">Log In</button>
+						<button className="btn btn-primary" onClick={() => window.sessionStorage.removeItem('accessToken')}>Log Out</button>
 					</Link>
 				</div>
 			</div>
