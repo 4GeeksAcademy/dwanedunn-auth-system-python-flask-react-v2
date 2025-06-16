8006485084e7f594a6a4c69eac809e974b71e4cd
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-16 18:31:23 +0000

    build create user route

--- src/api/routes.py
+++ src/api/routes.py
@@ -36,7 +36,31 @@ def create_token():
     access_token = create_access_token(identity=email_value)
     return jsonify(access_token=access_token), 200
 
-    # return jsonify(response_body), 200
+# Todo: Build the create user endpoint
+
+# first get the email and password from the request
+# use the email to check if the user already exists
+# if the user exists, return a message that the user already exists
+# if the user does not exist, create a new user and return the user data
+# hash the password before saving it to the database
+# return a message that the user was created successfully
+# return the user data in the response
+
+
+@api.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    email_value = data.get("email")
+    password_value = data.get("password")
+    if not email_value or not password_value:
+        return jsonify({"msg": "Bad email or password"}), 401
+    find_user = User.query.filter_by(email=email_value).first()
+    if find_user:
+        return jsonify({"message": "User already exists"}), 401
+    new_user = User(email=email_value, password=password_value, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 200
 
 
 @api.route('/private', methods=['GET'])
